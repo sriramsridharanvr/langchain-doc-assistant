8bd2db74ab5372cffc268802376ca018f81e9469
Sriram Sridharan <s.sriram@yethi.in>
2024-12-13 19:37:14 +0530

    initial commit after ingestion

---
+++ ingestion.py
@@ -0,0 +1,35 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+def ingest_docs():
+    loader = ReadTheDocsLoader("langchain-docs/api.python.langchain.com/en/latest", encoding="UTF-8")
+    raw_documents = loader.load()
+    print(f"Loaded {len(raw_documents)} documents")
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
+    documents = text_splitter.split_documents(raw_documents)
+
+    for doc in documents:
+        new_url = doc.metadata['source']
+        new_url = new_url.replace("langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to add {len(documents)} documents to Pinecone")
+
+    PineconeVectorStore.from_documents(
+        documents, embeddings, index_name=os.environ["INDEX_NAME"]
+    )
+
+    print("Loaded to vector store")
+
+if __name__ == "__main__":
+    ingest_docs()
